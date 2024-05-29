55b4b38c258f6a02b9c0676fc80e4d76bd68aec7
DimssOrigin <dimssfeibufight@gmail.com>
2024-05-29 11:46:31 +0700

    start

---
+++ database/migrations/2024_05_28_135803_create_donasis_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('donasis', function (Blueprint $table) {
+            $table->id();
+            $table->string("gambar");
+            $table->string("keterangan_donasi");
+            $table->string("jumlah_uang");
+            $table->string("nama_donasi");
+            $table->foreignIdFor(User::class);
+            $table->boolean("selesai");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('donasis');
+    }
+};
